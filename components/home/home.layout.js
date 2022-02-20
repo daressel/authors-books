99f32f8e1b5ef17d023425a901216efddfb717f1
daressel <nevaliashkaoxxxy@gmail.com>
2022-02-20 16:33:23 +0300

    install styled, add simple view for test

---
+++ components/home/home.layout.js
@@ -0,0 +1,8 @@
+import React, { memo } from "react";
+import { HomeWrapper } from "./home.styled";
+
+const HomeLayout = () => {
+  return <HomeWrapper>123123123123</HomeWrapper>;
+};
+
+export default memo(HomeLayout);
