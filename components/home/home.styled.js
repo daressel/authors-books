83cb639791c466bd297fee0a6099dc357aeb1a3b
daressel <nevaliashkaoxxxy@gmail.com>
2022-02-22 21:54:19 +0300

    Fix backgound image for author

--- components/home/home.styled.js
+++ components/home/home.styled.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import authorsBackground from '../../public/images/authors_background.jpg';
 
 export const HomeWrapper = styled.div`
   width: 60%;
@@ -8,37 +7,28 @@ export const HomeWrapper = styled.div`
   display: flex;
   align-items: center;
   flex-wrap: wrap;
-  background-image: url(${authorsBackground});
 `;
 
 export const AuthorWrapper = styled.div`
   width: 20%;
   min-width: 150px;
   height: 300px;
-  background-color: white;
   margin: 10px;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
-  border-radius: 5px;
+  background-image: url('/images/authors_background.jpg');
+  background-size: 300px;
+  border-radius: 10px;
   transition: all 0.2s;
   flex-grow: 1;
-  background-color: #b5b5b5;
   opacity: 1;
   color: white;
-
-  & > span {
-    display: none;
-  }
   :hover {
     box-shadow: 0 0 5px rgb(46, 46, 46);
     flex-grow: 2;
-    box-shadow: inset 0 0 0 1000px rgb(0 0 0 / 20%);
-
-    & > span {
-      display: unset;
-    }
+    box-shadow: inset 0 0 0 1000px rgb(0 0 0 / 50%);
   }
 `;
 
